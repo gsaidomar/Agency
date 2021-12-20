c284b60bcf385fd87a8220c609d6f8ebbba44fa7
gabriel <gabriel@saidomar.fr>
2021-12-20 13:23:36 +0100

    root homepage ok

---
+++ src/main/java/controller/index.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import com.google.gson.Gson;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import modele.Listing;
+
+/**
+ *
+ * @author kuncom
+ */
+@WebServlet(name = "index", urlPatterns = {"/index"})
+public class index extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+           URL url = new URL("http://localhost:8080/Agency/resources/flux/listing.json");
+        InputStreamReader reader = new InputStreamReader(url.openStream());
+        
+        BufferedReader br = new BufferedReader(reader);
+        String jsonString;
+        jsonString = br.readLine();
+       
+        Listing[] biens = new Gson().fromJson(jsonString, Listing[].class);
+        
+        ArrayList<Listing> lots = new ArrayList<Listing>();
+        for(int i = 0; i<biens.length; i++){
+            
+            lots.add(biens[i]);
+        
+        }
+       
+        HttpSession session = request.getSession();
+        session.setAttribute("biens", biens);
+        //session.setMaxInactiveInterval(200);
+
+
+            // Client side redirect
+            response.sendRedirect("home.jsp");
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
